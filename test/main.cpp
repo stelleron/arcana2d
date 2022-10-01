bcd5b88d36434da20293bb0394e6d8ebedb1460d
stelleron <shreyasdonti223@gmail.com>
2022-10-01 21:54:28 +0530

    Committing version 1.0.0

--- test/main.cpp
+++ test/main.cpp
@@ -2,19 +2,18 @@
 using namespace arcana;
 
 class UntitledGame : public Application{
-    Font font;
+
     void config(AppConfig& config) {
-        config.width = 1024;
+
     }
     void init(GameContext& ctx) {
-        font.load("cache/ThaleahFat.ttf");
 
     }
     void update(GameContext& ctx) {
 
     }
     void render(RenderContext& ctx) {
-        ctx.print(font, "Hello World!");
+
     }
     void finish() {
     
