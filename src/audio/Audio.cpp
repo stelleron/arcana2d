badab321ad1994b760cd3e4d1051ce57ba5d8a57
stelleron <shreyasdonti223@gmail.com>
2023-01-29 11:50:57 +0530

    Phasing out IrrKlang for microaudio

--- src/audio/Audio.cpp
+++ src/audio/Audio.cpp
@@ -1,15 +1,5 @@
 #include "audio/Audio.hpp"
 
 namespace arcana {
-    AudioContext::AudioContext() {
-        engine = createIrrKlangDevice();
-    }
 
-    AudioContext::~AudioContext() {
-        
-    }
-
-    void AudioContext::play(const char* path, bool looped) {
-        engine->play2D(path, looped);
-    }
 }
--- src/audio/Audio.hpp
+++ src/audio/Audio.hpp
@@ -1,16 +1,4 @@
 #ifndef ARCANA2D_AUDIO
     #define ARCANA2D_AUDIO
-    #include "irrKlang/irrKlang.h"
-    using namespace irrklang;
-
-    namespace arcana {
-        class AudioContext {
-            ISoundEngine* engine;
-            public:
-                AudioContext();
-                ~AudioContext();
-
-                void play(const char* path, bool looped = false);
-        };
-    }
+    
 #endif
